240525393b465e4d26fd3fec647e2e5509033ccd
s-arslaan <shaikharslaan1234@gmail.com>
2022-02-07 22:10:42 +0530

    nodemon success

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 const connectToMongo = require("./db");
+const express = require('express')
 
 connectToMongo();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Arslaan!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
