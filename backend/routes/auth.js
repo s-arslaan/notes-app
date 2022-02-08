c849c63723856b131b9a274e29c05b63c2cf5f1c
s-arslaan <shaikharslaan1234@gmail.com>
2022-02-08 23:13:35 +0530

    error handling (unique)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,34 +3,45 @@ const { body, validationResult } = require("express-validator");
 const User = require("../models/User");
 const router = express.Router();
 
-// Create User using: POST "api/auth/" Doesn't require auth
+// Create User using: POST "api/auth/createUser" NO LOGIN REQUIRED
 router.post(
-  "/",
+  "/createUser",
   [
     body("name", "Name too short!").isLength({ min: 3 }),
     body("email", "Invalid email!").isEmail(),
-    body("password", "Invalid password length!").trim().isLength({ min: 7, max: 16 }),
+    body("password", "Invalid password length!")
+      .trim()
+      .isLength({ min: 7, max: 16 }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    // error message sending
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    // console.log(req.body);
-    // const user = User(req.body);
-    // user.save();
-    // res.send(req.body);
 
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({ error: "Please enter unique email!", message: err.message });
+    try {
+      // checking if email exists
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "User already exists!" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
       });
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured");
+    }
+
+    // .then((user) => res.json(user))
+    // .catch((err) => {
+    //   console.log(err);
+    //   res.json({ error: "Please enter unique email!", message: err.message });
+    // });
   }
 );
 
