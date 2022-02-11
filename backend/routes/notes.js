fed3df05f3431fab731977ead532343f7ff88ae1
s-arslaan <shaikharslaan1234@gmail.com>
2022-02-11 21:07:16 +0530

    fetchnotes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const fetchuser = require("../middleware/fetchuser");
+const Notes = require("../models/Notes");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json([]);
+// ROUTE 3: Get loggedin User details using: POST "api/auth/getuser" LOGIN REQUIRED
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  const notes = await Notes.find({ user: req.user.id });
+  res.json(notes);
 });
 
 module.exports = router;
