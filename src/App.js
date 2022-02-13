7c7d9c8c0ec51cf0a174498a4a9cc7fe1f467f24
s-arslaan <shaikharslaan1234@gmail.com>
2022-02-13 22:07:20 +0530

    components & router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>THIS IS NOTES APP</h1>
+      <Router>
+        <Navbar />
+        {/* <LoadingBar height={3} color='#f11946' progress={this.state.progress}/> */}
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
