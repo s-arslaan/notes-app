e84884bfe84e72237944d451f6f49426ab264644
s-arslaan <shaikharslaan1234@gmail.com>
2022-12-11 17:44:17 +0530

    about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,31 @@
 import React from "react";
 
 const About = () => {
-
-  return <div>About Page</div>;
+    return (
+        <>
+            <h1 className="my-3">About NotesApp</h1>
+            <div className="card p-3 fs-5 bg-dark">
+                <p>
+                    NotesApp is a simple and secure application for taking and
+                    organizing notes. Our app uses secure APIs to ensure that
+                    your notes are always protected and accessible only to you.
+                </p>
+                <p>
+                    With NotesApp, you can easily create, edit, and organize
+                    notes on the go. Whether you're jotting down a grocery list,
+                    brainstorming ideas for a project, or writing down important
+                    information, Notes App makes it easy to capture your
+                    thoughts and keep them organized.
+                </p>
+                <p>
+                    Overall, NotesApp is the perfect tool for anyone who needs a
+                    simple, secure, and easy-to-use application for taking and
+                    organizing notes. Try it out today and see how it can help
+                    you stay organized and on top of your tasks.
+                </p>
+            </div>
+        </>
+    );
 };
 
 export default About;
